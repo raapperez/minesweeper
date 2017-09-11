56aa549f359601d5fc1514017c7d8f672b408aa3
rodrigo.perez <raapperez@gmail.com>
2017-09-11 19:17:15 -0300

    Implementing Minesweeper constructor

---
+++ src/minesweeper.rb
@@ -0,0 +1,21 @@
+
+class  Minesweeper
+    def initialize(width, height, num_mines)
+        @width = width
+        @height = height
+        @num_mines = num_mines
+    end
+
+    def width
+        @width
+    end
+
+    def height
+        @height
+    end
+
+    def num_mines
+        @num_mines
+    end
+    
+end
---
+++ test/minesweeper.rb
@@ -0,0 +1,14 @@
+require_relative "../src/minesweeper"
+require "test/unit"
+
+class TestMinesweeper < Test::Unit::TestCase
+
+def test_initialize
+    width, height, num_mines = 10, 20, 30
+    game = Minesweeper.new(width, height, num_mines)
+    assert_equal(width, game.width)
+    assert_equal(height, game.height)
+    assert_equal(num_mines, game.num_mines)
+end
+
+end
